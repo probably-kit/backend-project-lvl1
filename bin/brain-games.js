5e8f8a94a388062877bc3d49b926a774877d90d7
kit <probably.kit@gmail.com>
2021-11-07 14:56:48 +0300

    step 3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,3 @@
+import cli from '../src/cli.js';
 console.log('Welcome to the Brain Games!');
+cli();
---
+++ src/cli.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+export default () =>{
+    const name = readlineSync.question('May I have your name? ');
+    console.log('Hello, ' + name);
+}
