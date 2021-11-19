ad3cc6f34ac8e8177c516f2a8c91dbcd574f2c6a
kit <probably.kit@gmail.com>
2021-11-19 15:42:34 +0300

    step 7

---
+++ bin/brain-gcd.js
@@ -0,0 +1,50 @@
+import readlineSync from 'readline-sync';
+import {getRandom} from '../src/index.js';
+
+const getGCD = (FirstNumber, SecondNumber) => {
+    
+    if( FirstNumber > SecondNumber ){
+        for(let counter = SecondNumber; counter > 0; counter -= 1) {
+            if((FirstNumber % counter === 0) & (SecondNumber % counter === 0)){
+              const result = counter;
+                return result;
+            }
+        }
+
+    }
+    else if (FirstNumber === SecondNumber) return FirstNumber;
+    else {
+        for(let counter = FirstNumber; counter > 0; counter -= 1) {
+            if((FirstNumber % counter === 0) & (SecondNumber % counter === 0)){
+              const result = counter;
+                return result;
+            }
+        }
+    }
+
+    
+}
+
+
+export const BrainGCD = () =>{
+    const FirstNumber = getRandom();
+    const SecondNumber = getRandom();
+    const CorrectAnswer = getGCD(FirstNumber, SecondNumber );  
+    const Answer = readlineSync.question('Question: ' + FirstNumber +' & '+ SecondNumber + '\n' );
+    if(CorrectAnswer === Number(Answer)){
+        return true;
+    }
+    else{
+        console.log("'" + Answer + "'" + "is wrong answer :( Correct answer was '" + CorrectAnswer + "'" ); 
+        return false;
+        
+    }
+
+
+      
+    
+
+
+
+
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 import cli from './cli.js';
 import {BrainCalc} from '../bin/brain-calc.js';
 import {BrainEven} from '../bin/brain-even.js';
-
+import {BrainGCD} from '../bin/brain-gcd.js';
 
 export const getRandom = () => {
     const result = Math.round(Math.random() * 100);
@@ -12,20 +12,49 @@ export const getRandom = () => {
 console.log('Welcome to the Brain Games!');
 const name = cli();
 
-const games = ['Brain-Even','Brain-Calc'];
+const games = ['Brain-Even','Brain-Calc','Brain-GCD'];
 
-let CurrentGame;
+let CurrentGame = true;
 
 for(const game of games){
     
-    let counter = 0;
+    let counter = 0; 
+
+    if(CurrentGame === false){
+        console.log("Let's try again, " + name + "!");
+        break;
+    }
+
+
+    switch(game){
+        case 'Brain-Even': 
+        console.log('Answer "yes" if the number is even, otherwise answer "no".');              
+        break;
+
+        case 'Brain-Calc': 
+        console.log('What is the result of the expression?');
+        break;
+
+        case 'Brain-GCD': 
+        console.log('Find the greatest common divisor of given numbers');
+        break;
+        default: console.log(':('); break;
+    }
+
+    
     for(; counter < 3; counter += 1){
+    
         switch(game){
-            case 'Brain-Even': CurrentGame = BrainEven();
-            console.log('Answer "yes" if the number is even, otherwise answer "no".');                    
+            case 'Brain-Even': 
+            CurrentGame = BrainEven();               
             break;
-            case 'Brain-Calc': CurrentGame = BrainCalc();
-            console.log('What is the result of the expression?');
+    
+            case 'Brain-Calc': 
+            CurrentGame = BrainCalc();
+            break;
+    
+            case 'Brain-GCD': 
+            CurrentGame = BrainGCD();
             break;
             default: console.log(':('); break;
         }
@@ -33,10 +62,10 @@ for(const game of games){
         if(CurrentGame === true){
             console.log('Correct!');
         }
-        else{
-            console.log("Let's try again, " + name + "!");
+        else { 
             break;
-        }   
+        }
+
     }
 
     if( counter === 3){
